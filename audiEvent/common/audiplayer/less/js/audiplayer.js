cb6f9deed4e73a26b5bcc53c78003a5cb0f5196a
HSflower <2983rgt@naver.com>
2016-09-05 02:05:53 +0900

    Add files via upload - next, common/css
    audi event page common(audiplayer source)

---
+++ audiEvent/common/audiplayer/less/js/audiplayer.js
@@ -0,0 +1,24 @@
+/* script.js - 스크립트, 2012 © yamoo9.com
+---------------------------------------------------------------- */
+jQuery(function($) {
+	
+	// 비디오 플레이어 플러그인 적용.
+	$('#audiplayer').videoPlayer({
+		change_theme: false,
+                color : 'red'
+	});
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+

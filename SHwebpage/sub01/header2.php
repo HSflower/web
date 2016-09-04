c80cf4f469a25f2bb0c2e0ba11069ea1342b54d9
HSflower <2983rgt@naver.com>
2016-09-05 02:20:31 +0900

    shwebpage img sources2
    Add files via upload

---
+++ SHwebpage/sub01/header2.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+<head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="./css/font-awesome.min.css" rel="stylesheet" type="text/css">
+        <link href="./css/bootstrap.css" rel="stylesheet" type="text/css">
+        <link href="./css/common.css" rel="stylesheet" type="text/css">
+        <link href="./font/font.css" type="text/css">
+        <script type="text/javascript" src="./js/jquery.min.js"></script>
+        <script type="text/javascript" src="./js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="./js/jquery.js"></script>
+		<script type="text/javascript" src="./js/common.js"></script>
+    </head>
+    
+    <body>
+      <div class="section">
+            <div class="container">
+                <div class="row" style="margin-top:0px!important;margin-bottom:0px!important;">
+                    <div class="col-md-12 text-right" style="padding-right:0px!important;">
+                        <div class="btn-group">
+                            <a href="#" class="btn btn-default top">로그인</a>
+                            <a href="#" class="btn btn-default top">회원가입</a>
+                            <a href="#" class="btn btn-default top">오시는 길</a>
+                            <a href="#" class="btn btn-default top">공식 블로그</a>
+                            <a href="#" class="btn btn-default top">공식 페이스북</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="row" id="mainDiv" style="margin-top:0px!important;background-color:black;">
+                <div class="col-md-2" style="background-color:black;padding-left:0px!important;padding-right:0px!important;">
+                        <!-- <img src="./main/sidebar.png" style="position:absolute;z-index:-1000; width:100%;" class="img-responsive"/> -->
+                        <a href="main.php"><img src="./main/logo.png" class="img-responsive" style="z-index:100"/></a>
+                        <ul class="list-group" style="background-color:;">
+                            <a href="sub1.php"><li class="list-group-item"><img src="./main/Introduction.png" style="margin-left:10%;"/></li></a>
+                            <a href="#"><li class="list-group-item"><img src="./main/Beauty Program.png" style="margin-left:10%;"/></li></a>
+                            <a href="sub3.php"><li class="list-group-item"><img src="./main/community.png" style="margin-left:10%;"/></li></a>
+                            <a href="sub4.php"><li class="list-group-item"><img src="./main/Sohyang Event.png" style="margin-left:10%;"/></li></a>
+                            <a href="sub5.php"><li class="list-group-item"><img src="./main/Reservation.png" style="margin-left:10%;"/></li></a>
+                            <a href="#"><li class="list-group-item"><img src="./main/Interview.png" style="margin-left:10%;"/></li></a>
+                        </ul>
+                        <div id="floatdiv">
+						    <ul> 
+						   		
+						   </ul>
+						</div>
+						<div id='quick' style="position:fixed;margin-left: 12px;">
+							<img src="./main/leftquick.png"  />
+						</div>
+						
+						
+                    </div>
+                    <div class="col-md-10" style="background-color:whitesmoke;padding-left:0px!important;padding-right:0px!important;position: relative;">
+                    <div id="" style="margin-left:100%; position:fixed;right:240px; z-index: 100">
+    					<ul>
+    						<li><a href="#"><img src="./sub00/top.png" /></a></li>
+    						<li><a href="#"><img src="./sub00/loginbtn_side.png" /></a></li>
+    						<li><a href="#"><img src="./sub00/joinbtn_side.png" /></a></li>
+    						<li><a href="#"><img src="./sub00/eventbtn_side.png" /></a></li>
+    						<li><a href="#"><img src="./sub00/qnabtn_side.png" /></a></li>
+    						<li><a href="#"><img src="./sub00/blogbtn_side.png" /></a></li>
+    						<li><a href="#"><img src="./sub00/facebookbtn_side.png" /></a></li>
+    					</ul>
+    				</div> 
